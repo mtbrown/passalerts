6b521b71c506c98a8ab8b9eda234772982f68603
Mark Brown <mark@mtbrown.me>
2016-09-06 17:48:52 -0700

    Use bs4 for faster section parsing

--- passmonitor.py
+++ passmonitor.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from collections import namedtuple
 from tabulate import tabulate
+from bs4 import BeautifulSoup, NavigableString
 import logging
 
 PASS_URL = "http://pass.calpoly.edu/main.html"
@@ -9,20 +10,23 @@ QUARTER = "Fall"
 WATCH_LIST = ["EE 308", "ENGL 134", "CPE 464", "CPE 329"]
 
 
-Section = namedtuple("Section", "course section type number professor available enrolled waiting")
+Section = namedtuple("Section", "course section type id professor available enrolled waiting")
 
 
 def main():
     logging.basicConfig(format='%(asctime)s: [%(levelname)s] %(message)s', level=logging.INFO)
 
     driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(10)  # wait 10 seconds for elements to load for entire session
     init_session(driver, PASS_URL, QUARTER)
 
     logging.info("Selecting courses")
     select_courses(driver, WATCH_LIST)
+
     logging.info("Parsing sections")
-    section_info = parse_sections(driver)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    section_info = parse_sections(soup)
+
     logging.info("Finished")
 
     print_sections(section_info)
@@ -54,21 +58,31 @@ def select_courses(driver, course_list):
     driver.find_element_by_css_selector(".right.btn.btn-next").click()  # Continue to choose sections
 
 
-def parse_sections(driver):
+def parse_sections(soup):
     section_info = dict.fromkeys(WATCH_LIST)
 
-    selected_courses = driver.find_elements_by_xpath("//div[@class='select-course']")
+    selected_courses = soup.find_all(class_="select-course")
     for course in selected_courses:
         course_name = course.text.split('-')[0].strip()
+        course_name = " ".join(course_name.split())  # remove duplicate spaces
         if course_name not in WATCH_LIST:
             continue  # Skip classes that aren't in watch list, e.g. labs that are auto added
 
         section_info[course_name] = []
-        for section in course.find_elements_by_xpath("./table/tbody/tr"):
-            info = [elem.text for elem in section.find_elements_by_xpath("./td[not(.//input)]")]
-            if len(info) < 5:
-                continue  # Skip table rows that aren't sections, e.g. "Section Notes"
-            section_info[course_name].append(Section(course_name, *info[0:7]))
+        for section in course.find_all("table"):
+            for row in section.tbody:
+                if isinstance(row, NavigableString) or row.find(class_="section-notes"):
+                    continue  # skip rows containing section notes
+
+                col = row.find(class_="sectionNumber")
+                if col is None:
+                    continue
+
+                info = []
+                for _ in range(len(Section._fields) - 1):
+                    info.append(col.text.strip())
+                    col = col.find_next_sibling("td")
+                section_info[course_name].append(Section(course_name, *info))
 
     return section_info
 
