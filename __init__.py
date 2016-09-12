c8701449e9bfd23256962e7bbbbd9a45a822d9a5
Mark Brown <mark@mtbrown.me>
2016-09-11 18:21:01 -0700

    Completed event checking, modified checkers to yield events rather than return booleans

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ import sys
 from scrape import scrape_sections, print_sections
 from events import check_events
 from config import parse_config, parse_subscriptions
+from notifications import notify
 
 DB_FILE = 'pass.db'
 CONFIG_FILE = 'pass.conf'
@@ -19,12 +20,13 @@ def main():
 
     subscriptions = parse_subscriptions(config)
 
-    cur_sections = scrape_sections(subscriptions.keys(), config['Settings']['quarter'])
-    print_sections(cur_sections)
+    cur_courses = scrape_sections(subscriptions.keys(), config['Settings']['quarter'])
+    print_sections(cur_courses)
 
-    with shelve.open(DB_FILE) as prev_sections:
-        check_events(subscriptions, prev_sections, cur_sections)
-        prev_sections.update(cur_sections)
+    with shelve.open(DB_FILE) as prev_courses:
+        for event in check_events(subscriptions, prev_courses, cur_courses):
+            notify(config, event.message)
+        prev_courses.update(cur_courses)
 
 
 if __name__ == '__main__':
--- events.py
+++ events.py
@@ -1,6 +1,8 @@
-def check_events(events, prev_sections, cur_sections):
-    pass
+import logging
+from collections import namedtuple
 
+
+Event = namedtuple('Event', 'event message course section')
 
 def check_available(prev_sections, cur_sections):
     """
@@ -8,13 +10,12 @@ def check_available(prev_sections, cur_sections):
     seats available in the previous section list, False is returned to prevent multiple
     notification triggers.
     """
-    for id, section in prev_sections.items():
-        if section.available > 0:
-            return False
     for id, section in cur_sections.items():
         if section.available > 0:
-            return True
-    return False
+            # Ignore if the section was previously available
+            if id in prev_sections and prev_sections[id].available > 0:
+                continue
+            yield (section, section.available)
 
 
 def check_new_section(prev_sections, cur_sections):
@@ -23,17 +24,45 @@ def check_new_section(prev_sections, cur_sections):
     """
     for id, section in cur_sections.items():
         if id not in prev_sections:
-            return True
-    return False
+            yield (section, section.id)
 
 
 def check_instructor_change(prev_sections, cur_sections):
     """
     Checks if the instructor has been modified for any of the sections.
     """
-    for id, section in prev_sections:
+    for id, section in prev_sections.items():
         if id not in cur_sections:
             continue  # section was removed since last update
         if section.instructor != cur_sections[id].instructor:
-            return True
-    return False
+            yield (section, section.instructor)
+
+
+event_checkers = {
+    'available': check_available,
+    'new_section': check_new_section,
+    'instructor_change': check_instructor_change
+}
+
+
+humanize_event = {
+    'available': "{extra} seat(s) are now available in section {section} of {course}.",
+    'new_section': "A new section was added for {course}: {extra}",
+    'instructor_change': "The instructor for section {section} of {course} was changed to {extra}.",
+}
+
+
+def check_events(subscriptions, prev_courses, cur_courses):
+    for course, subscription in subscriptions.items():
+        if course not in prev_courses:
+            continue  # if course was just added, can't check for changes
+
+        for event in subscription.events:
+            if event not in event_checkers:
+                logging.warning('Unrecognized event "{0}" in {1} subscription'.format(event, course))
+                continue
+
+            for section, extra in event_checkers[event](prev_courses[course], cur_courses[course]):
+                logging.info("Detected event: {0}, {1}, {2}".format(event, course, section))
+                message = humanize_event[event].format(section=section.section, course=course, extra=extra)
+                yield Event(event, message, course, section)
